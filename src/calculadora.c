e26d105289fb920dcbc3ae65099a3519ef9b251c
Jesus Avila <jesus.avila.05@gmail.com>
2023-06-05 22:25:33 -0300

    Agregado Formato

--- src/calculadora.c
+++ src/calculadora.c
@@ -1,18 +1,19 @@
 /************************************************************************************************
 Copyright (c) 2023, Jesus Gerardo Daniel Avila jesus.avila.05@gmail.com
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial
-portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
-OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-SPDX-License-Identifier: MIT
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions: The above copyright notice and this
+permission notice shall be included in all copies or substantial portions of the
+Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. SPDX-License-Identifier: MIT
 *************************************************************************************************/
 /**
  * \cond
@@ -25,122 +26,134 @@ SPDX-License-Identifier: MIT
  ** \brief Calculadora Dinámica
  ** @{ */
 
-/* === Headers files inclusions =============================================================== */
+/* === Headers files inclusions
+ * =============================================================== */
 
 #include "calculadora.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-/* === Macros definitions ====================================================================== */
 
-/* === Private data type declarations ========================================================== */
+/* === Macros definitions
+ * ====================================================================== */
+
+/* === Private data type declarations
+ * ========================================================== */
 
 /// @brief Descriptor a estructura operacion_s
-typedef struct operacion_s * operacion_t;
+typedef struct operacion_s *operacion_t;
 
 /// @brief Contiene un puntero al primer elemento de la lista enlazada
 struct calculadora_s {
-    operacion_t operaciones;
+  operacion_t operaciones;
 };
 
 /// @brief Definición de los atruibutos de operacion_s
 struct operacion_s {
-    char operador;
-    funcion_t funcion;
-    operacion_t siguiente;
+  char operador;
+  funcion_t funcion;
+  operacion_t siguiente;
 };
 
-/* === Private variable declarations =========================================================== */
+/* === Private variable declarations
+ * =========================================================== */
 
-/* === Private function declarations =========================================================== */
+/* === Private function declarations
+ * =========================================================== */
 
 /**
  * @brief Busca la operacion en la lista enlazada
  *
  * @param calculadora Descriptor de calculadora
  * @param operador Indicador de la operacion a buscar
- * @return operacion_t: Descriptor de la operacion encontrada. Si no existe devuelve NULL
+ * @return operacion_t: Descriptor de la operacion encontrada. Si no existe
+ * devuelve NULL
  */
 operacion_t BuscarOperacion(calculadora_t calculadora, char operador);
 
-/* === Public variable definitions ============================================================= */
+/* === Public variable definitions
+ * ============================================================= */
 
-/* === Private variable definitions ============================================================ */
+/* === Private variable definitions
+ * ============================================================ */
 
-/* === Private function implementation ========================================================= */
+/* === Private function implementation
+ * ========================================================= */
 
 calculadora_t CrearCalculadora(void) {
-    calculadora_t calculadora = malloc(sizeof(struct calculadora_s));
-    if (calculadora) {
-        memset(calculadora, 0, sizeof(struct calculadora_s));
-    }
-    return calculadora;
+  calculadora_t calculadora = malloc(sizeof(struct calculadora_s));
+  if (calculadora) {
+    memset(calculadora, 0, sizeof(struct calculadora_s));
+  }
+  return calculadora;
 }
 
+bool AgregarOperacion(calculadora_t calculadora, char operador,
+                      funcion_t funcion) {
+  operacion_t operacion = NULL;
 
-bool AgregarOperacion(calculadora_t calculadora, char operador, funcion_t funcion) {
-    operacion_t operacion = NULL;
-
-    if (!BuscarOperacion(calculadora, operador)) {
-        operacion = malloc(sizeof(struct operacion_s));
-    }
-    if (operacion) {
-        operacion->operador = operador;
-        operacion->funcion = funcion;
-        operacion->siguiente = calculadora->operaciones;
-        calculadora->operaciones = operacion;
-    }
-    return operacion;
+  if (!BuscarOperacion(calculadora, operador)) {
+    operacion = malloc(sizeof(struct operacion_s));
+  }
+  if (operacion) {
+    operacion->operador = operador;
+    operacion->funcion = funcion;
+    operacion->siguiente = calculadora->operaciones;
+    calculadora->operaciones = operacion;
+  }
+  return operacion;
   //  return  (operacion != NULL);
 }
 
 static operacion_t BuscarOperacion(calculadora_t calculadora, char operador) {
-    operacion_t result = NULL;
-    operacion_t actual = calculadora->operaciones;
+  operacion_t result = NULL;
+  operacion_t actual = calculadora->operaciones;
 
-    if (actual != NULL) {
-        for (actual; actual != NULL; actual = actual->siguiente) {
-            if (actual->operador == operador) {
-                result = actual;
-               
-                break;
-            }
-        }
+  if (actual != NULL) {
+    for (actual; actual != NULL; actual = actual->siguiente) {
+      if (actual->operador == operador) {
+        result = actual;
+
+        break;
+      }
     }
+  }
 
-    return result;
+  return result;
 }
 
-int Calcular(calculadora_t calculadora, char * cadena) {
-    int a, b;
-    char operador;
-    int resultado = 0;
-    operacion_t operacion = NULL;
+int Calcular(calculadora_t calculadora, char *cadena) {
+  int a, b;
+  char operador;
+  int resultado = 0;
+  operacion_t operacion = NULL;
 
-    for (int index = 0; index < strlen(cadena); index++) {
-        if (cadena[index] < '0') {
-            operador = cadena[index];
-            a = atoi(cadena);
-            b = atoi(cadena + index + 1);
-            break;
-        }
+  for (int index = 0; index < strlen(cadena); index++) {
+    if (cadena[index] < '0') {
+      operador = cadena[index];
+      a = atoi(cadena);
+      b = atoi(cadena + index + 1);
+      break;
     }
+  }
 
-    operacion = BuscarOperacion(calculadora, operador);
-    if (operacion) {
-        resultado = operacion->funcion(a, b);
-    }
+  operacion = BuscarOperacion(calculadora, operador);
+  if (operacion) {
+    resultado = operacion->funcion(a, b);
+  }
 
-    return resultado;
+  return resultado;
 }
 
-/* === Public function implementation ========================================================== */
+/* === Public function implementation
+ * ========================================================== */
 
 /**
  * \endcond
  */
 
-/* === End of documentation ==================================================================== */
+/* === End of documentation
+ * ==================================================================== */
 
 /** @} End of module definition for doxygen */
--- src/main.c
+++ src/main.c
@@ -1,18 +1,19 @@
 /************************************************************************************************
 Copyright (c) 2023, Jesus Gerardo Daniel Avila jesus.avila.05@gmail.com
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial
-portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
-OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-SPDX-License-Identifier: MIT
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions: The above copyright notice and this
+permission notice shall be included in all copies or substantial portions of the
+Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. SPDX-License-Identifier: MIT
 *************************************************************************************************/
 
 /**
@@ -22,25 +23,32 @@ SPDX-License-Identifier: MIT
 
 /** \brief Programa de ejemplo para el uso de calculadora.h
  **
- ** En este archivo se presenta el modo de hacer un correcto uso de la librería calculadora.h.
- ** Se realizarán las operaciones básicas de suma, resta, multiplicación y division como ejemplo de
+ ** En este archivo se presenta el modo de hacer un correcto uso de la librería
+ *calculadora.h.
+ ** Se realizarán las operaciones básicas de suma, resta, multiplicación y
+ *division como ejemplo de
  ** aplicacion.
  **
  ** \addtogroup Aplicacion
  ** @{ */
 
-/* === Headers files inclusions =============================================================== */
+/* === Headers files inclusions
+ * =============================================================== */
 
-#include <stdio.h>
 #include "calculadora.h"
+#include <stdio.h>
 
-/* === Macros definitions ====================================================================== */
+/* === Macros definitions
+ * ====================================================================== */
 
-/* === Private data type declarations ========================================================== */
+/* === Private data type declarations
+ * ========================================================== */
 
-/* === Private variable declarations =========================================================== */
+/* === Private variable declarations
+ * =========================================================== */
 
-/* === Private function declarations =========================================================== */
+/* === Private function declarations
+ * =========================================================== */
 
 /**
  * @brief Funcion definida por el usuario
@@ -78,62 +86,63 @@ int Producto(int a, int b);
  */
 int Cociente(int a, int b);
 
-/* === Public variable definitions ============================================================= */
+/* === Public variable definitions
+ * ============================================================= */
 
-/* === Private variable definitions ============================================================ */
+/* === Private variable definitions
+ * ============================================================ */
 
-/* === Private function implementation ========================================================= */
+/* === Private function implementation
+ * ========================================================= */
 
-int Suma(int a, int b) {
-    return (a + b);
-}
+int Suma(int a, int b) { return (a + b); }
 
-int Resta(int a, int b) {
-    return (a - b);
-}
+int Resta(int a, int b) { return (a - b); }
 
-int Producto(int a, int b) {
-    return (a * b);
-}
+int Producto(int a, int b) { return (a * b); }
 
-int Cociente(int a, int b) {
-    return (a / b);
-}
+int Cociente(int a, int b) { return (a / b); }
 
-/* === Public function implementation ========================================================== */
+/* === Public function implementation
+ * ========================================================== */
 
 int main(void) {
 
-    int resultado = 0;
-    /// Creo la calculadora
-    calculadora_t mi_calculadora = CrearCalculadora();
+  int resultado = 0;
+  /// Creo la calculadora
+  calculadora_t mi_calculadora = CrearCalculadora();
 
-    // Agregar operaciones
-    AgregarOperacion(mi_calculadora, '+', Suma);
-    AgregarOperacion(mi_calculadora, '+', Resta);
-    AgregarOperacion(mi_calculadora, '*', Producto);
-    AgregarOperacion(mi_calculadora, '/', Cociente);
+  // Agregar operaciones
+  AgregarOperacion(mi_calculadora, '+', Suma);
+  AgregarOperacion(mi_calculadora, '+', Resta);
+  AgregarOperacion(mi_calculadora, '*', Producto);
+  AgregarOperacion(mi_calculadora, '/', Cociente);
 
-    // Realizo los calculos e imprimo por pantalla los resultados,
-    resultado = Calcular(mi_calculadora, "14+5");
-    printf("14 + 5 = %i\n", resultado);
+  // Realizo los calculos e imprimo por pantalla los resultados,
+  resultado = Calcular(mi_calculadora, "14+5");
 
-    resultado = Calcular(mi_calculadora, "20-10");
-    printf("20 - 10 = %i\n", resultado);
+  printf("14 + 5 = %i\n", resultado);
 
-    resultado = Calcular(mi_calculadora, "3*5");
-    printf("3 x 5 = %i\n", resultado);
+  resultado = Calcular(mi_calculadora, "20-10");
+  
+  printf("20 - 10 = %i\n", resultado);
 
-    resultado = Calcular(mi_calculadora, "789614385/5");
-    printf("789614385 / 5 = %i\n", resultado);
+  resultado = Calcular(mi_calculadora, "3*5");
+  
+  printf("3 x 5 = %i\n", resultado);
 
-    return 0;
+  resultado = Calcular(mi_calculadora, "789614385/5");
+ 
+ printf("789614385 / 5 = %i\n", resultado);
+
+  return 0;
 }
 
 /**
  * \endcond
  */
 
-/* === End of documentation ==================================================================== */
+/* === End of documentation
+ * ==================================================================== */
 
 /** @} End of module definition for doxygen */
